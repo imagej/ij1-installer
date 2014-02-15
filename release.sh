3a1cd0cc3c6754cdce5776313a05374e552977e0
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-02-15 17:21:57 -0600

    Use newest launcher snapshot versions
    Eventually, we should auto-detect the newest version, maybe switch away
    from snapshot versions to release ones altogether, but for now, let's
    punt.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- release.sh
+++ release.sh
@@ -56,7 +56,7 @@ WINDOWS_64_LAUNCHER_URL=$WINDOWS_BUILD_URL/artifact/ImageJ1-win64.exe
 LAUNCHER_JOB_URL=$JENKINS_URL/job/ImageJ-launcher
 LAUNCHER_BUILD_URL=$LAUNCHER_JOB_URL/label=master/lastSuccessfulBuild
 LAUNCHER_NAR_URL=$LAUNCHER_BUILD_URL/artifact/target/nar/
-PATTERN=ij-launcher-2.0.3-SNAPSHOT-X-gcc-executable/bin/X-gcc/ij-launcher
+PATTERN=ij-launcher-2.0.4-SNAPSHOT-X-gcc-executable/bin/X-gcc/ij-launcher
 LAUNCHER_URL_PATTERN=$LAUNCHER_NAR_URL/$PATTERN
 
 get_launcher_url () {
